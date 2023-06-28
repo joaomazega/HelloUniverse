f1b4eac80b06fc63d794157935d5210d9905435c
jppma <jppma@PCDOJOTA>
2023-06-28 15:45:52 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+/*
+	Jay Mazega
+	CIS-1202.201
+	June 28th, 2023
+	Source Code Management 1
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, Universe!" << endl;
+	return 0;
+}
